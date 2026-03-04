becff4c9bdc4d2f547772e5da27a0789f67fdd0e
darthamendoas <miguel.amendoeira@ort.org.br>
2026-03-04 15:35:23 -0300

    initial

---
+++ 1.py
@@ -0,0 +1,7 @@
+nome=input("Qual é o nome:")
+classe=input("Qual é a classe:")
+nivel=int(input("Qual é o nivel:"))
+
+print("Nome:......................",nome)
+print("Classe:......................",classe)
+print("Nivel:......................",nivel)
